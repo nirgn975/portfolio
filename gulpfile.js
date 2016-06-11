142406799eeb2dc1c6c57f59174e84441e793d7c
Nir Galon <nirgn975@gmail.com>
2016-06-11 15:16:33 +0300

    Fix javascript, and stiky the nav.

--- gulpfile.js
+++ gulpfile.js
@@ -19,12 +19,14 @@ var messages = {
  */
 gulp.task('javascript', function(){
   return gulp.src([
-    './assets/javascript/semantic.min.js',
-  ])
+      './assets/javascript/tools/semantic.min.js',
+      './assets/javascript/custom/nav.js'
+    ])
     .pipe(concat('app.min.js'))
     .pipe(uglify())
+    .pipe(gulp.dest('./_site/assets/javascript'))
     .pipe(browserSync.reload({stream:true}))
-    .pipe(gulp.dest('./_site/assets/javascript'));
+    .pipe(gulp.dest('assets/javascript'));
 });
 
 /**
@@ -83,8 +85,11 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/scss/**', ['sass']);
-    gulp.watch('assets/javascript/**', ['javascript']);
+    gulp.watch([
+      'assets/scss/*/*.scss',
+      'assets/scss/*.scss',
+    ], ['sass']);
+    gulp.watch('assets/javascript/*/*', ['javascript']);
     gulp.watch([
       'index.html',
       '_layouts/*.html',
