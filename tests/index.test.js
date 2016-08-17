90371377998acaeb0076e8a448004714c5482f04
Nir Galon <nirgn975@gmail.com>
2016-08-17 09:21:58 +0300

    Add example test.

---
+++ tests/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+// var index = require('./../index');
+
+describe('index', function() {
+  it('should work', function() {
+    expect(true).to.be.true;
+  });
+});
