96843daa852eeff6cb4331103e4ff5b7029b4639
Nir Galon <nirgn975@gmail.com>
2016-08-17 09:48:25 +0300

    Remove mocha and chai.

--- tests/index.test.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect;
-// var index = require('./../index');
-
-describe('index', function() {
-  it('should work', function() {
-    expect(true).to.be.true;
-  });
-});
