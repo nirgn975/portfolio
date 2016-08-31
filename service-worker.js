e0b78e273be43167906e5308b8a645b70e634404
Nir Galon <nirgn975@gmail.com>
2016-08-31 09:27:26 +0300

    Add service worker.

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+importScripts('/cache-polyfill.js');
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('airhorner').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.html?homescreen=1',
+        '/?homescreen=1',
+        '/assets/scss/main.min.css',
+        '/assets/app.min.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
