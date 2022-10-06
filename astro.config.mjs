cf537360f1c315fc114bf9544d1b7337d5de67ad
Nir Galon <nir@galons.io>
2022-10-06 15:51:43 +0300

    fix: asset url path

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nir.galons.io",
-  base: "/portfolio",
   integrations: [sitemap(), tailwind(), astroImageTools, partytown(), mdx()],
 });
