db0346a1b7501196ffb0ac8fd325a92743e8591a
Nir Galon <nir@galons.io>
2025-03-15 22:42:06 +0200

    fix: cleanup

--- app/error.vue
+++ app/error.vue
@@ -1,44 +1,23 @@
 <script setup lang="ts">
-import type { NuxtError } from '#app'
+import type { NuxtError } from "#app";
 
 defineProps({
   error: {
     type: Object as PropType<NuxtError>,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 
 useHead({
   htmlAttrs: {
-    lang: 'en'
-  }
-})
+    lang: "en",
+  },
+});
 
 useSeoMeta({
-  title: 'Page not found',
-  description: 'We are sorry but this page could not be found.'
-})
-
-const { data: navigation } = await useAsyncData('navigation', () => queryCollectionNavigation('docs'), {
-  transform: data => data.find(item => item.path === '/docs')?.children || []
-})
-const { data: files } = useLazyAsyncData('search', () => queryCollectionSearchSections('docs'), {
-  server: false
-})
-
-const links = [{
-  label: 'Docs',
-  icon: 'i-lucide-book',
-  to: '/docs/getting-started'
-}, {
-  label: 'Pricing',
-  icon: 'i-lucide-credit-card',
-  to: '/pricing'
-}, {
-  label: 'Blog',
-  icon: 'i-lucide-pencil',
-  to: '/blog'
-}]
+  title: "Page not found",
+  description: "We are sorry but this page could not be found.",
+});
 </script>
 
 <template>
@@ -54,16 +33,6 @@ const links = [{
     </UMain>
 
     <AppFooter />
-
-    <ClientOnly>
-      <LazyUContentSearch
-        :files="files"
-        shortcut="meta_k"
-        :navigation="navigation"
-        :links="links"
-        :fuse="{ resultLimit: 42 }"
-      />
-    </ClientOnly>
 
     <UToaster />
   </div>
